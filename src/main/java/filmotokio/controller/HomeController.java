2fab895c34309f4fc505fa29229fb2f2aa82a959
Kiniame Carvalho <KiniameCarvalho@KiniameCarvalho.lan>
2026-03-30 08:10:45 +0100

    little update

--- src/main/java/filmotokio/controller/HomeController.java
+++ src/main/java/filmotokio/controller/HomeController.java
@@ -44,6 +44,6 @@ public class HomeController {
         model.addAttribute("films", films);
         model.addAttribute("filmAverages", filmAverages);
 
-        return "home";
+        return "index";
     }
 }
